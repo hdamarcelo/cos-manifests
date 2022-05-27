24258d23282855fb08957985b1e2a51375e7b018
Marcelo Henrique Diniz de Araujo <51032022+hdamarcelo@users.noreply.github.com>
2022-05-27 18:58:37 -0300

    fix: bundle generation
    - Add skipRange
    - Use correct camel-k and strimzi versions
    - Use sha for camel an strimzi

--- hack/bundle.sh
+++ hack/bundle.sh
@@ -25,39 +25,81 @@ if [ ! -d "${DIR_OVERLAY}" ]; then
     exit 2
 fi
 
-echo "##############################################"
-echo "# id      : ${BUNDLE_ID}"
-echo "# version : ${ADDON_VERSION}"
-echo "# dir     : ${BUNDLE_DIR}"
-echo "# package : ${BUNDLE_NAME}"
-echo "# base    : ${BUNDLE_BASE}"
-echo "# channel : ${BUNDLE_CHANNEL}"
-echo "##############################################"
+function bundle_external_operators() {
+  local kustomization=$1
+  local container_image=''
+  local version=''
 
-kustomize build "${DIR_OVERLAY}" | operator-sdk generate bundle \
+  container_image=$(printf '%s\n' "$kustomization" | yq 'select(.kind=="Deployment").spec.template.spec.containers[0].image')
+  version=$(printf '%s' "$container_image" | cut -f 2 -d ':' | tr '[:upper:]' '[:lower:]')
+  version=${version:0:25}
+  container_image_name=$(printf '%s' "$container_image" | cut -f 1 -d ':')
+  container_image_sha256=$(skopeo inspect "docker://$container_image" --format "{{.Digest}}")
+
+  DIR_ADDON="${DIR_ADDON}/${version}"
+
+  printf '%s\n' "$kustomization" | operator-sdk generate bundle \
     --package "${BUNDLE_NAME}" \
-    --channels ${BUNDLE_CHANNEL} \
-    --default-channel ${BUNDLE_CHANNEL} \
+    --channels "${BUNDLE_CHANNEL}" \
+    --default-channel "${BUNDLE_CHANNEL}" \
+    --output-dir "${DIR_ADDON}" \
+    --version "${version}" \
+    --kustomize-dir "${DIR_OVERLAY}"
+    
+    container_image_new_name="$container_image_name@$container_image_sha256" \
+    yq -i \
+    '.spec.install.spec.deployments[0].spec.template.spec.containers[0].image=strenv(container_image_new_name)'\
+     "${DIR_ADDON}/manifests/${BUNDLE_NAME}.clusterserviceversion.yaml"
+
+    yq -i 'del(.spec.install.spec.deployments[].label)' \
+    "${DIR_ADDON}/manifests/${BUNDLE_NAME}.clusterserviceversion.yaml"
+}
+
+function bundle() {
+  local kustomization=$1
+  printf '%s\n' "$kustomization" | operator-sdk generate bundle \
+    --package "${BUNDLE_NAME}" \
+    --channels "${BUNDLE_CHANNEL}" \
+    --default-channel "${BUNDLE_CHANNEL}" \
     --output-dir "${DIR_ADDON}" \
     --version "${ADDON_VERSION}" \
     --kustomize-dir "${DIR_OVERLAY}"
+}
 
-rm bundle.Dockerfile
+function main() {
+  echo "##############################################"
+  echo "# id      : ${BUNDLE_ID}"
+  echo "# version : ${ADDON_VERSION}"
+  echo "# dir     : ${BUNDLE_DIR}"
+  echo "# package : ${BUNDLE_NAME}"
+  echo "# base    : ${BUNDLE_BASE}"
+  echo "# channel : ${BUNDLE_CHANNEL}"
+  echo "##############################################"
 
-yq -i 'del(.annotations."operators.operatorframework.io.metrics.builder")' \
-  "${DIR_ADDON}/metadata/annotations.yaml"
-yq -i 'del(.annotations."operators.operatorframework.io.metrics.mediatype.v1")' \
-  "${DIR_ADDON}/metadata/annotations.yaml"
-yq -i 'del(.annotations."operators.operatorframework.io.metrics.project_layout")' \
-  "${DIR_ADDON}/metadata/annotations.yaml"
-yq -i 'del(.annotations."operators.operatorframework.io.test.mediatype.v1")' \
-  "${DIR_ADDON}/metadata/annotations.yaml"
-yq -i 'del(.annotations."operators.operatorframework.io.test.config.v1")' \
-  "${DIR_ADDON}/metadata/annotations.yaml"
+  kustomization=$(kustomize build "${DIR_OVERLAY}")
 
-yq -i 'del(.metadata.annotations."operators.operatorframework.io/builder")' \
-  "${DIR_ADDON}/manifests/${BUNDLE_NAME}.clusterserviceversion.yaml"
-yq -i 'del(.metadata.annotations."operators.operatorframework.io/project_layout")' \
-  "${DIR_ADDON}/manifests/${BUNDLE_NAME}.clusterserviceversion.yaml"
-yq -i 'del(.spec.icon[0])' \
-  "${DIR_ADDON}/manifests/${BUNDLE_NAME}.clusterserviceversion.yaml"
+  if [[ "${BUNDLE_ID}" = "strimzi"* ]] || [[ "${BUNDLE_ID}" = "camel-k"* ]]; then
+    bundle_external_operators "$kustomization"
+  else
+    bundle "$kustomization"
+  fi
+
+  rm bundle.Dockerfile
+
+  yq -i 'del(.annotations."operators.operatorframework.io.metrics.builder")
+        | del(.annotations."operators.operatorframework.io.metrics.mediatype.v1")
+        | del(.annotations."operators.operatorframework.io.metrics.project_layout")
+        | del(.annotations."operators.operatorframework.io.test.mediatype.v1")
+        | del(.annotations."operators.operatorframework.io.test.config.v1")
+        ' \
+    "${DIR_ADDON}/metadata/annotations.yaml"
+
+
+  yq -i 'del(.metadata.annotations."operators.operatorframework.io/builder")
+        | del(.metadata.annotations."operators.operatorframework.io/project_layout")
+        | del(.spec.icon[0])
+        ' \
+    "${DIR_ADDON}/manifests/${BUNDLE_NAME}.clusterserviceversion.yaml"
+}
+
+main "@"
